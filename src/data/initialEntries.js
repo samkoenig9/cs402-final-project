f1cc12a57f4f3219b15b9072740cf6bc83c90b2a
Parker Smith <126126738+smithbparker@users.noreply.github.com>
2026-05-04 11:15:50 -0600

    Add dodgeball opponent for fight 3

--- src/data/initialEntries.js
+++ src/data/initialEntries.js
@@ -1,7 +1,7 @@
 export const initialEntries = [
   { id: 'fight-1', label: '1 - Hamster', detail: 'Just an ordinary hamster?' },
   { id: 'fight-2', label: '2 - Boxer', detail: 'Quick hands and light feet.' },
-  { id: 'fight-3', label: '3 - Fighter', detail: 'Placeholder 3' },
+  { id: 'fight-3', label: '3 - White Goodman', detail: 'Fast throws with tricky timing.' },
   { id: 'fight-4', label: '4 - Fighter', detail: 'Placeholder 4' },
   { id: 'fight-5', label: '5 - Fighter', detail: 'Placeholder 5' },
 ];
