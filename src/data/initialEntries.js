9952dd4d049359ffe93eae924d836d27b876bdda
Riordin Dupree <riordindupree@u.boisestate.edu>
2026-05-05 18:37:13 -0600

    updated initialEntries to match fightData

--- src/data/initialEntries.js
+++ src/data/initialEntries.js
@@ -2,6 +2,6 @@ export const initialEntries = [
   { id: 'fight-1', label: '1 - Hamster', detail: 'Just an ordinary hamster?' },
   { id: 'fight-2', label: '2 - Boxer', detail: 'Quick hands and light feet.' },
   { id: 'fight-3', label: '3 - White Goodman', detail: 'Fast throws with tricky timing.' },
-  { id: 'fight-4', label: '4 - Fighter', detail: 'Placeholder 4' },
-  { id: 'fight-5', label: '5 - Fighter', detail: 'Placeholder 5' },
+  { id: 'fight-4', label: '4 - The Dentom', detail: 'Full Of Suprises!' },
+  { id: 'fight-5', label: '5 - Quapok', detail: 'My Beloved Little Freaky Ducky Darling!' },
 ];
